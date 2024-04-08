9e211a817c4f750c0fe9a31c3b5ebdd38f6aca61
JakubMatuszak240749 <240749@edu.p.lodz.pl>
2024-04-08 17:53:32 +0200

    Added test for CommandCounterVisitor

--- src/test/java/edu/kis/powp/jobs2d/events/SelectRunCurrentCommandOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectRunCurrentCommandOptionListener.java
@@ -4,20 +4,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.command.IDriverCommand;
+import edu.kis.powp.jobs2d.command.visitor.CommandCounterVisitor;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
 
 public class SelectRunCurrentCommandOptionListener implements ActionListener {
 
-    private DriverManager driverManager;
+    private final DriverManager driverManager;
+    private final CommandCounterVisitor commandCounterVisitor;
 
-    public SelectRunCurrentCommandOptionListener(DriverManager driverManager) {
+    public SelectRunCurrentCommandOptionListener(DriverManager driverManager, CommandCounterVisitor commandCounterVisitor) {
         this.driverManager = driverManager;
+        this.commandCounterVisitor = commandCounterVisitor;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         IDriverCommand command = CommandsFeature.getCommandManager().getCurrentCommand();
         command.execute(driverManager.getCurrentDriver());
+        command.accept(commandCounterVisitor);
     }
 }

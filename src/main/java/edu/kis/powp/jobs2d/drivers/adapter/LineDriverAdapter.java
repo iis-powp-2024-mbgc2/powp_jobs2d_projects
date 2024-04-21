5ef41f21aa467b2cb71860eb4262102bdde799b8
Bartosz Chadryś <bartekch322@gmail.com>
2024-04-21 18:24:09 +0200

    Add printing usage

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
@@ -3,16 +3,21 @@ package edu.kis.powp.jobs2d.drivers.adapter;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.Job2dDriverMonitor;
+
+import java.util.logging.Logger;
 
 /**
  * Line adapter - Job2dDriver with DrawPanelController object.
  */
-public class LineDriverAdapter implements Job2dDriver {
+public class LineDriverAdapter implements Job2dDriver, Job2dDriverMonitor {
     private ILine line;
     private int startX = 0, startY = 0;
     private String name;
 
     private DrawPanelController drawController;
+
+    private final double distance = 0.0;
 
     public LineDriverAdapter(DrawPanelController drawController, ILine line, String name) {
         super();
@@ -34,10 +39,18 @@ public class LineDriverAdapter implements Job2dDriver {
         line.setEndCoordinates(x, y);
 
         drawController.drawLine(line);
+
+        this.printUsage();
     }
 
     @Override
     public String toString() {
         return "2d device simulator - " + name;
+    }
+
+    @Override
+    public void printUsage() {
+        Logger logger =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.info(String.valueOf(distance));
     }
 }

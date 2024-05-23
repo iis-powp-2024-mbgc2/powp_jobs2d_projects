08dcfd2273d5ec29524238d37db98f1f95f3b5f7
Gujansko <mateusz.klos150401@gmail.com>
2024-05-23 11:13:38 +0200

    Remove redundancy for RectangularCanvas

--- src/main/java/edu/kis/powp/jobs2d/command/canvas/CanvasA3.java
+++ src/main/java/edu/kis/powp/jobs2d/command/canvas/CanvasA3.java
@@ -1,26 +1,10 @@
 package edu.kis.powp.jobs2d.command.canvas;
 
-import edu.kis.powp.jobs2d.command.CompoundCommand;
-import edu.kis.powp.jobs2d.command.builder.CompoundCommandBuilder;
 
-public class CanvasA3 implements Canvas {
-    private static final int WIDTH = 297;
-    private static final int HEIGHT = 420;
 
-    @Override
-    public CompoundCommand getCanvasCommand() {
-        CompoundCommandBuilder builder = new CompoundCommandBuilder();
-        builder.setName("Canvas A3")
-                .addSetPosition(-WIDTH / 2, -HEIGHT / 2)
-                .addOperateTo(-WIDTH / 2, HEIGHT / 2)
-                .addOperateTo(WIDTH / 2, HEIGHT / 2)
-                .addOperateTo(WIDTH / 2, -HEIGHT / 2)
-                .addOperateTo(-WIDTH / 2, -HEIGHT / 2);
-        return builder.build();
-    }
+public class CanvasA3 extends RectangularCanvas {
 
-    @Override
-    public boolean isInsideCanvas(int x, int y) {
-        return x >= -WIDTH / 2 && x <= WIDTH / 2 && y >= -HEIGHT / 2 && y <= HEIGHT / 2;
+    public CanvasA3() {
+        super(297, 420, "A3");
     }
 }

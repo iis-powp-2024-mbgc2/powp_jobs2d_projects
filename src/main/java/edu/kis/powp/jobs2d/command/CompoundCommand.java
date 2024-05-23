6d99b76668cb981577b3a49ddaeae8a815710260
Bartosz Chadryś <bartekch322@gmail.com>
2024-05-23 12:46:50 +0200

    Add immutable implementation of CompoundCommand and refactor code

--- src/main/java/edu/kis/powp/jobs2d/command/CompoundCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/CompoundCommand.java
@@ -1,57 +1,19 @@
 package edu.kis.powp.jobs2d.command;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Logger;
-
-public class CompoundCommand implements ICompoundCommand {
-    private List<DriverCommand> commands = new ArrayList<>();
-    private final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+public final class CompoundCommand implements ICompoundCommand {
+    private final List<DriverCommand> commands;
     private final String name;
 
-    public CompoundCommand(String name) {
-        this.name = name;
-    }
-
     public CompoundCommand(List<DriverCommand> commands, String name) {
-        this.commands = commands;
+        this.commands = new ArrayList<>(commands);
         this.name = name;
-    }
-
-    public void addCommand(DriverCommand command) {
-        commands.add(command);
-    }
-
-    public void addCommand(DriverCommand command, int index) {
-        try {
-            commands.add(index, command);
-        } catch (IndexOutOfBoundsException e) {
-            logger.warning("Index out of bounds");
-        }
-    }
-
-    public void removeCommand(DriverCommand command) {
-        commands.remove(command);
-    }
-
-    public void removeCommand(int index) {
-        try {
-            commands.remove(index);
-        } catch (IndexOutOfBoundsException e) {
-            logger.warning("Index out of bounds");
-        }
-    }
-
-    public void clearCommand() {
-        commands.clear();
     }
 
     public List<DriverCommand> getCommands() {
-        return commands;
+        return Collections.unmodifiableList(commands);
     }
 
     @Override

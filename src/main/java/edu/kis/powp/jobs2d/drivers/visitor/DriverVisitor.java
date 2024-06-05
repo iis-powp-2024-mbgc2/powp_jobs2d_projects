bd961a33ef9e06fe049accb04a44d8d56d2f84bb
exaggeratedrumors <exaggeratedrumors@gmail.com>
2024-06-05 17:48:44 +0200

    add visitor interface

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/DriverVisitor.java
@@ -0,0 +1,14 @@
+package edu.kis.powp.jobs2d.drivers.visitor;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.DriversComposite;
+import edu.kis.powp.jobs2d.drivers.LoggerDriver;
+import edu.kis.powp.jobs2d.drivers.RecordingDriverDecorator;
+import edu.kis.powp.jobs2d.drivers.UsageMonitorDriverDecorator;
+
+public interface DriverVisitor {
+    void visit(LoggerDriver loggerDriver);
+    void visit(DriversComposite driverComposite);
+    void visit(RecordingDriverDecorator recordingDriverDecorator);
+    void visit(UsageMonitorDriverDecorator usageMonitorDriverDecorator);
+}

69aeb88d8a31033864076fb5d4edc99fa7d9f1e1
240844 <mekiroriginal@gmail.com>
2024-06-06 13:42:43 +0200

    Added the ability to add new points.

--- src/main/java/edu/kis/powp/jobs2d/drivers/MouseClickEditor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/MouseClickEditor.java
@@ -6,9 +6,11 @@ import edu.kis.powp.jobs2d.command.DriverCommand;
 import edu.kis.powp.jobs2d.command.ICompoundCommand;
 import edu.kis.powp.jobs2d.command.OperateToCommand;
 import edu.kis.powp.jobs2d.command.SetPositionCommand;
+import edu.kis.powp.jobs2d.command.builder.CompoundCommandBuilder;
 import edu.kis.powp.jobs2d.events.MouseClickListener;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.swing.*;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
@@ -34,8 +36,8 @@ public class MouseClickEditor extends MouseClickConverter implements MouseClickL
         if(buttonPressed == MOUSE_BUTTON_LEFT) {
 
             List<Point> points = new ArrayList<>();
-            compoundCommand.iterator().forEachRemaining(command->{
-                Point point = new Point((command).getX(),(command).getY());
+            compoundCommand.iterator().forEachRemaining(command -> {
+                Point point = new Point((command).getX(), (command).getY());
                 points.add(point);
                 //System.out.println("operate" + point);
             });
@@ -44,13 +46,53 @@ public class MouseClickEditor extends MouseClickConverter implements MouseClickL
 
             //selectedPoint.ifPresent(System.out::println);
 
-            if(selectedPoint.isPresent()) {
+            if (selectedPoint.isPresent()) {
                 System.out.println("found point: " + selectedPoint.get());
                 this.selectedCommands = findCommands(selectedPoint.get(), compoundCommand.iterator());
-                System.out.println("all commands with the point: "+ selectedCommands);
+                System.out.println("all commands with the point: " + selectedCommands);
 
             }
-
+        } else if (buttonPressed == MOUSE_BUTTON_MID){
+            CompoundCommandBuilder commandBuilder = new CompoundCommandBuilder();
+            System.out.println("Position: " + position);
+            List<Point> points = new ArrayList<>();
+            compoundCommand.iterator().forEachRemaining(command -> {
+                Point point = new Point((command).getX(), (command).getY());
+                points.add(point);
+            });
+            System.out.println(points);
+            Optional<Point> nearest = points.stream().min(Comparator.comparingDouble(point -> point.distance(position)));
+            System.out.println(nearest);
+            System.out.println(nearest);
+            if (nearest.isPresent()) {
+                SetPositionCommand setPosition = new SetPositionCommand(position.x, position.y);
+                OperateToCommand operateTo = new OperateToCommand(position.x, position.y);
+                this.selectedCommands = findCommands(nearest.get(), compoundCommand.iterator());
+                List<DriverCommand> commands = new ArrayList<>();
+                Iterator<DriverCommand> iterator = compoundCommand.iterator();
+                System.out.println(selectedCommands);
+                int index = 0;
+                boolean found = false;
+                while (iterator.hasNext()) {
+                    DriverCommand command = iterator.next();
+                    commands.add(command);
+                    if (!found && command.getX() == nearest.get().x && command.getY() == nearest.get().y) {
+                        found = true;
+                        index = commands.size();
+                    }
+                }
+                if (found) {
+                    commands.add(index, operateTo);
+                } else {
+                    commands.add(operateTo);
+                }
+                commands.forEach(command -> {
+                    commandBuilder.addCommand(command);
+                });
+                drawPanelController.clearPanel();
+                compoundCommand = commandBuilder.build();
+                compoundCommand.execute(driver);
+            }
         } else if (buttonPressed == MOUSE_BUTTON_RIGHT) {
             selectedCommands.forEach(command -> {
                 command.setX(position.x);

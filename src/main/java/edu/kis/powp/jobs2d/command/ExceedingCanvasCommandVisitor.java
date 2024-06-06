73bf8f27e679e7913b40d8306dd728010090038a
MidZ <mondechju@wp.pl>
2024-06-06 12:51:19 +0200

    ExceedingCanvasCommandVisitor now has methods to check for exceeding

--- src/main/java/edu/kis/powp/jobs2d/command/ExceedingCanvasCommandVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ExceedingCanvasCommandVisitor.java
@@ -1,14 +1,20 @@
 package edu.kis.powp.jobs2d.command;
 
 import javax.swing.*;
-import java.awt.Dimension;
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class ExceedingCanvasCommandVisitor implements CommandVisitor {
 
     private JPanel drawArea;
     private static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    private boolean isExceedingCanvas = false;
+    private ArrayList<Point> commands = new ArrayList<>();
 
     public ExceedingCanvasCommandVisitor(JPanel drawArea) {
         this.drawArea = drawArea;
@@ -18,34 +24,35 @@ public class ExceedingCanvasCommandVisitor implements CommandVisitor {
         return drawArea.getSize();
     }
 
-    @Override
-    public void visit(OperateToCommand operateToCommand) {
+    private Point commandCoordinatesToCanvasCoordinates(int x, int y) {
         Dimension size = getDrawAreaSize();
         int width = size.width;
         int height = size.height;
+        x += width / 2;
+        y = height / 2 - y;
+        return new Point(x, y);
+    }
+    private void checkForExceedingCanvas(int x, int y) {
 
-        int x = operateToCommand.getX() + width / 2;
-        int y = height / 2 - operateToCommand.getY();
-
-        if (x >= width || x < 0 || y >= height || y < 0) {
-            logger.warning("Exceeding canvas: (" + x + ", " + y + ")");
+        Dimension size = getDrawAreaSize();
+        Point p = commandCoordinatesToCanvasCoordinates(x, y);
+        x = p.x;
+        y = p.y;
+        if (x >= size.width || x < 0 || y >= size.height || y < 0) {
+            isExceedingCanvas = true;
+            commands.add(new Point(x, y));
         }
     }
+    @Override
+    public void visit(OperateToCommand operateToCommand) {
+        checkForExceedingCanvas(operateToCommand.getX(), operateToCommand.getY());
+        }
+
 
     @Override
     public void visit(SetPositionCommand setPositionCommand) {
-        Dimension size = getDrawAreaSize();
-        int width = size.width;
-        int height = size.height;
-
-        int x = setPositionCommand.getX() + width / 2;
-        int y = height / 2 - setPositionCommand.getY();
-
-
-        if (x >= width || x < 0 || y >= height || y < 0) {
-            logger.warning("Exceeding canvas: (" + x + ", " + y + ")");
+        checkForExceedingCanvas(setPositionCommand.getX(), setPositionCommand.getY());
         }
-    }
 
     @Override
     public void visit(ICompoundCommand compoundCommand) {
@@ -53,5 +60,13 @@ public class ExceedingCanvasCommandVisitor implements CommandVisitor {
         while (it.hasNext()) {
             it.next().accept(this);
         }
+    }
+
+    public boolean isExceedingCanvas() {
+        return isExceedingCanvas;
+    }
+    public List<Point> getPointsExceeding()
+    {
+        return commands;
     }
 }

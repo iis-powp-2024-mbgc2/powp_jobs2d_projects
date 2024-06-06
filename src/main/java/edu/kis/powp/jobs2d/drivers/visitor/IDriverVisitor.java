c12c53c0e0c2643c206743cae5a721b150268c4c
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-06-06 02:32:55 +0200

    Create IDriverVisitor.java

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IDriverVisitor.java
@@ -0,0 +1,25 @@
+package edu.kis.powp.jobs2d.drivers.visitor;
+
+import edu.kis.powp.jobs2d.drivers.*;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDriverAdapter
+import edu.kis.powp.jobs2d.drivers.transformators.TransformingJob2dDriverDecorator;
+
+public interface IDriverVisitor {
+
+    int getCount();
+
+    void visit(DriversComposite driversComposite);
+
+    void visit(LoggerDriver loggerDriver);
+
+    void visit(LineDriverAdapter lineDriverAdapter);
+
+    void visit(RealTimeDecoratorDriver realTimeDecoratorDriver);
+
+    void visit(UsageMonitorDriverDecorator usageMonitorDriverDecorator);
+
+    void visit(RecordingDriverDecorator recordingDriverDecorator);
+
+    void visit(TransformingJob2dDriverDecorator transformingJob2dDriverDecorator);
+
+}

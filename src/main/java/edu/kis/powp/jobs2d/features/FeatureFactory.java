9a985dbc62de1d68be52be5666fce619ce79e64e
Matenar <240736@edu.p.lodz.pl>
2024-09-19 12:08:25 +0200

    changed FeatureFactory to accomadate simpler management

--- src/main/java/edu/kis/powp/jobs2d/features/FeatureFactory.java
+++ src/main/java/edu/kis/powp/jobs2d/features/FeatureFactory.java
@@ -1,16 +1,49 @@
 package edu.kis.powp.jobs2d.features;
 
 import edu.kis.powp.appbase.Application;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public abstract class FeatureFactory {
 
-    public Feature create(Application app) {
-        Feature feature = createFeature();
-        feature.setup(app);
-        return feature;
+    private static Map<String, Feature> featuresRegistry = new HashMap<>();
+
+    public static void addFeature(Feature feature) {
+        featuresRegistry.put(feature.getClass().getSimpleName(), feature);
     }
 
-    protected abstract Feature createFeature();
+    public static void addFeature(Class<? extends Feature> featureClass) {
+        try {
+            Feature feature = featureClass.getDeclaredConstructor().newInstance();
+            featuresRegistry.put(featureClass.getSimpleName(), feature);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to instantiate feature: " + featureClass.getSimpleName(), e);
+        }
+    }
+
+    public static void initializeAllFeatures(Application app) {
+        for (Feature feature : featuresRegistry.values()) {
+            feature.setup(app);
+        }
+    }
+
+
+    public static void setupFeature(String featureName, Application app) {
+        Feature feature = featuresRegistry.get(featureName);
+        if (feature != null) {
+            feature.setup(app);
+        } else {
+            throw new IllegalArgumentException("Feature not found: " + featureName);
+        }
+    }
+
+    public static void clearFeatures() {
+        featuresRegistry.clear();
+    }
+
+    public static void listFeatures() {
+        featuresRegistry.keySet().forEach(System.out::println);
+    }
 }
 
